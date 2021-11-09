c6956909b0f1fdec2758fa5375857646ccffdac3
Aron Palmsaar <apalmsaar@gmail.com>
2021-11-09 15:38:43 +0200

    Add project files.

---
+++ Colors/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your personality defining color (Red, Blue, Green, Yellow)");
+            string userColor = Console.ReadLine();
+            Console.WriteLine(GetPersonalityData(userColor));
+        }
+        public static string[] ReadPersonalityFile()
+        {
+            string filePath = @"C:\Users\Laptop\samples\personality.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            return dataFromFile;
+        }
+        public static string GetPersonalityData(string userInput)
+        {
+            string[] horoscope = ReadPersonalityFile();
+            string personality = "";
+            foreach (string element in horoscope)
+            {
+                if (element.Contains(userInput))
+                {
+                    personality = element;
+                }
+
+            }
+            return personality;
+        }
+    }
+}
